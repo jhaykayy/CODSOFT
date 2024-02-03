021a4e5f0f35128b5f3f5bc52560f114a5746b08
joshua <jkweknow@gmail.com>
2024-02-03 23:19:32 +0100

    added all the gaming items

--- landingpage/landingpage.js
+++ landingpage/landingpage.js
@@ -3,3 +3,28 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 0)
 })
+
+/* filterable card */
+
+let list = document.querySelectorAll(".list");
+let card = document.querySelectorAll(".card");
+
+for(let i = 0 ; i < list.length; i++) {
+  list[i].addEventListener("click", function () {
+    for(j = 0; j<list.length; j++) {
+      list[j].classList.remove("active");
+    }
+    this.classList.add("active");
+
+    let dataFilter = this.getAttribute("data-filter");
+
+    for (let k = 0; k<card.length; k++) {
+      card[k].classList.remove("active");
+      card[k].classList.add("hide");
+
+      if (card[k].getAttribute("data-item") == dataFilter || dataFilter == "all")
+      card[k].classList.remove("hide");
+      card[k].classList.add("active");
+    }
+  })
+}
